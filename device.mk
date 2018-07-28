f903ddd426fb2e36c3d93bf66a9248243740bb5f
Aaron Kling <webgeek1234@gmail.com>
2018-07-28 15:20:08 -0500

    shieldtablet: remove hwui properties
    Change-Id: I02308c614bd02fd46e5181b738edf4a0ef110837

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ $(call inherit-product, device/nvidia/shieldtablet/device_tablet.mk)
 $(call inherit-product, device/nvidia/shield-common/shield.mk)
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 ifneq ("$(wildcard vendor/nvidia/shield/shieldtablet.mk)","")
 $(call inherit-product, vendor/nvidia/shield/shieldtablet.mk)
